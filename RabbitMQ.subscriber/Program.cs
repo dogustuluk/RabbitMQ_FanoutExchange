ef37972e35822fad8efbf7c81f05909590fc7679
dogustuluk <dogus.tuluk@gmail.com>
2022-05-06 14:36:02 +0300

    notes added

--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -7,7 +7,12 @@ using System.Threading;
 namespace RabbitMQ.subscriber
 {
     class Program
-    {
+    { //Producer (publisher) rabbitMQ'ya bir mesaj gönderdiğinde exchange tipi fanout olan exchange'e mesajı gönderir. 
+        //Fanout exchange ise herhangi bir filtreleme yapmadan kendisine bağlı olan tüm kuyruklara mesajı iletir.
+        //Yani üretilen bir mesaj tüm kuyruklara ayrı ayrı yollanır.
+        //örnek olarak rest servis olmadan hava tahmin durumu. producer saat başı hava tahmin durumunu alıyor ve bunu fanout exchange'e gönderiyor. eğer kuyruk yoksa
+        //          tüm mesajlar boşa gidiyor. Bu senaryoda fanout exchange'e kuyruk consumer tarafında oluşturulmalı çünkü kaç tane consumer'ın bağlanacağını bilmiyoruz.
+        //          Fanout exchange tipinde genellikle kuyrukları consumer'lar oluşturur.
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory();
